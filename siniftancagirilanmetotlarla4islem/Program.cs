2c8228d793b156a5243b18a9d4988d224bc53ba9
efetekin481@outlook.com <efetekin481@outlook.com>
2023-03-20 22:44:32 +0300

    Add project files.

---
+++ siniftancagirilanmetotlarla4islem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace siniftancagirilanmetotlarla4islem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dortislem islemler = new dortislem();
+            islemler.Dortislem(50, 40);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ siniftancagirilanmetotlarla4islem/dortislem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace siniftancagirilanmetotlarla4islem
+{
+    internal class dortislem
+    {
+        public int Dortislem(int s1, int s2)
+        {
+            int toplamsonuc = s1 + s2;
+            int cikarmasonuc = s1 - s2;
+            int carpmasonuc = s1 * s2;
+            int bolmesonuc = s1 / s2;
+
+            int sonuc = toplamsonuc + cikarmasonuc + carpmasonuc + bolmesonuc;
+
+            Console.WriteLine("Toplama Sonucu: " + toplamsonuc +  "\n" + "Çıkarma Sonucu: " + cikarmasonuc + "\n" + "Çarpma Sonucu: " + carpmasonuc + "\n" + "Bölme Sonucu: " + bolmesonuc);
+            return sonuc;
+            
+        }
+    }
+}
